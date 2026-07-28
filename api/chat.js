8dbe616849516aa8549630339a0e5474059088af
Pollibizneswomen <polina8934@icloud.com>
2026-07-28 04:40:36 +0200

    Обновлена модель Gemini

--- api/chat.js
+++ api/chat.js
@@ -42,7 +42,7 @@ ${message}
         `.trim();
 
         const geminiResponse = await fetch(
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent",
             {
                 method: "POST",
                 headers: {
